9fd3bc054dd12c2da8350742db2907965869f0c2
Peny825 <peny835@gmail.com>
2025-03-06 19:25:49 +0100

    Baseplate for next.js project.

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-9xl">Home</h1>
+    </main>
+  );
+}
